0e1e7862897eadb77436f473f1f323e768832f39
kueke <ernwu.kuek@gmail.com>
2023-04-23 21:05:26 +0800

    Q2

--- Question2.java
+++ Question2.java
@@ -5,11 +5,9 @@ public class Question2
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-    System.out.print("Weight (kg): ");
     double weight = in.nextDouble();
-    System.out.print("Height (m): ");
     double height = in.nextDouble();
     double BMI = weight / (height * height);
-    System.out.print("The BMI is " + BMI);
+    System.out.print(BMI);
   }
 }
